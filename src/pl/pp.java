0994d3c5f946464dbf078d9bf05995a63d805b6a
Lucjusz44 <gafarovabubakr005@gmail.com>
2025-04-14 00:48:36 +0200

    myFifthApp.java

--- src/pl/pp.java
+++ src/pl/pp.java
@@ -1,7 +1,33 @@
-package pl;
+public class myFifthApp {
 
-public class pp {
+    public static void sayHello() {
+        System.out.println("Hello from a method!");
+    }
+
+    public static void showSum(int a, int b) {
+        System.out.println("Sum: " + (a + b));
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void printCharGrid(char ch, int cols, int rows) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello Basics of Programming");
+        // Task 1 (demo)
+        sayHello();
+        showSum(2, 3);
+        System.out.println("Multiply: " + multiply(4, 5));
+
+        // Task 2 (your method)
+        printCharGrid('#', 5, 3);
     }
 }
