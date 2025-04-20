25aefdbe9f4cb55246a6436f40376568d0797663
Lucjusz44 <gafarovabubakr005@gmail.com>
2025-04-21 00:27:05 +0200

    myFifthApp.java

--- src/pl/pp.java
+++ src/pl/pp.java
@@ -1,7 +1,31 @@
-package pl;
-
-public class pp {
+public class myFifthApp {
     public static void main(String[] args) {
-        System.out.println("Hello Basics of Programming");
+        int n = 10;
+
+        long startIter = System.nanoTime();
+        long iterResult = factorialIterative(n);
+        long endIter = System.nanoTime();
+
+        long startRec = System.nanoTime();
+        long recResult = factorialRecursive(n);
+        long endRec = System.nanoTime();
+
+        System.out.println("Iterative Result: " + iterResult);
+        System.out.println("Recursive Result: " + recResult);
+        System.out.println("Iterative Time: " + (endIter - startIter) + " ns");
+        System.out.println("Recursive Time: " + (endRec - startRec) + " ns");
+    }
+
+    public static long factorialIterative(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static long factorialRecursive(int n) {
+        if (n <= 1) return 1;
+        return n * factorialRecursive(n - 1);
     }
 }
