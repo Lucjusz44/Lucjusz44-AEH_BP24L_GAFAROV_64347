08efa59b96a1bb9b6bcf967e38b7a153763d0999
Lucjusz44 <gafarovabubakr005@gmail.com>
2025-05-04 21:03:10 +0200

    mySeventhApp.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class mySeventhApp {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Create a new Person object
+        Person p = new Person("John", 30, "Warsaw", 1995);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Show initial data
+        System.out.println("Before aging:");
+        p.showPersonData();
+
+        // Grow older by 10 years
+        p.growOld(10);
+
+        // Be younger by 1 year
+        p.beYounger();
+
+        // Show updated data
+        System.out.println("\nAfter modifications:");
+        p.showPersonData();
+    }
+}
+
+class Person {
+    String name;
+    int age;
+    String address;
+    int year_of_birth;
+
+    // Constructor
+    public Person(String name, int age, String address, int year_of_birth) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.year_of_birth = year_of_birth;
+    }
+
+    // Method to display person's data
+    public void showPersonData() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Year of Birth: " + year_of_birth);
+    }
+
+    // Method to increase age by given years
+    public void growOld(int years) {
+        this.age += years;
+    }
+
+    // Method to decrease age by 1
+    public void beYounger() {
+        this.age -= 1;
     }
 }
